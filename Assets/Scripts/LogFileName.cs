1f8fdfc53e140902e6060b72c1951ebf72c22ebe
zhangzena <1403538149@qq.com>
2021-05-14 21:57:09 +0800

    生成文件名

--- Assets/Scripts/LogFileName.cs
+++ Assets/Scripts/LogFileName.cs
@@ -1,3 +1,20 @@
-﻿#if UNITY_EDITOR
+﻿using UnityEngine;
+using System;
+
+#if UNITY_EDITOR
 	using UnityEditor;
 #endif
+
+namespace QFramework
+{
+	public static class LogFileName
+	{
+		#if UNITY_EDITOR
+			[MenuItem("QFramework/生成UnityPackage名字")]
+		#endif
+
+		private static void GenerateUnityPackageName(){
+			Debug.Log("QFrameword_" + DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+		}
+	}
+}
