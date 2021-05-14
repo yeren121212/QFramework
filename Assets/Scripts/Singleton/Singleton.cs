9910294c219c81dac9b5f7d7f1fc4c55029dad99
zhangzena <1403538149@qq.com>
2021-05-14 23:33:24 +0800

    单例的模板

---
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace QFramework
+{
+	public abstract class Singleton<T> where T : Singleton<T> {
+		protected static T instance = null;
+
+		protected Singleton() {}
+
+		public static T Instance()
+		{
+			if(instance == null){
+				// 获取所有非public的构造函数
+				ConstructorInfo[] ctors = typeof(T).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+				// 获取无参的构造方法
+				ConstructorInfo ctor = Array.Find(ctors, c => c.GetParameters().Length == 0);
+				if(ctor == null){
+					throw new Exception("Non-public ctor() not found!");
+				}
+				// 调用构造方法
+				instance = ctor.Invoke(null) as T;
+			}
+
+			return instance;
+		}
+	}
+}
---
+++ Assets/Scripts/Singleton/XXManager.cs
@@ -0,0 +1,19 @@
+﻿using QFramework;
+using UnityEngine;
+
+// 继承Singleton
+// 实现非public的构造方法
+
+public class XXManager : Singleton<XXManager>
+{
+	private XXManager()
+	{
+		Debug.Log("XXManager构造函数");
+	}
+
+	public void Func()
+	{
+		Debug.Log("XXManager:Func");
+	}
+
+}
